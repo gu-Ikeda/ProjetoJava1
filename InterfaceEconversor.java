b1a726a21a2fc6229309e8571f6f00aae613743d
gu-Ikeda <gustavokikeda@gmail.com>
2025-03-21 10:03:03 -0300

    Create InterfaceEconversor.java

---
+++ InterfaceEconversor.java
@@ -0,0 +1,102 @@
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        Pilha pilha = new Pilha();
+        char resp = 'S';
+        char[] junto = new char[20];
+        char[] vari = new char[10];
+        int[] valor = new int[10];
+        char[] posfixa = new char[20];
+        int convertido;
+        int tracker = 0, reach = 0, aux = 0;
+        while(resp == 'S'){
+            String entrada = scan.nextLine();
+            entrada = entrada.replaceAll("\\s", ""); //remove os espacos
+            junto = entrada.toCharArray(); // converte para um array de char
+            if(junto[1] == '=')
+            {
+                for(int i = 0;i<junto.length;i++)
+                {
+                    vari[tracker] = junto[0];
+                    convertido = Integer.parseInt(String.valueOf(junto[2]));
+                    valor[tracker] = convertido;
+                }
+                System.out.println(vari[tracker]+" = "+valor[tracker]);
+                tracker++;
+            }
+            else if(entrada.equals("VARS"))
+            {
+                if(tracker == 0)
+                {
+                    System.out.println("Nenhuma variavel iniciada");
+                }
+                else
+                {
+                    for(int j = 0;j<tracker;j++)
+                    {
+                        System.out.println(vari[j]+" = "+valor[j]);
+                    }
+                }
+            }
+            else if(entrada.equals("RESET"))
+            {
+                vari = new char[10];
+                valor = new int[10];
+                posfixa = new char[20]; //da pra reiniciar as variaveis no final do else
+                System.out.println("Variaveis reiniciadas");
+                tracker = 0;
+                reach = 0;
+                aux = 0;
+            }
+            else if(entrada.equals("EXIT"))
+            {
+                resp = 'N';
+            }
+            else
+            {
+                while(reach < junto.length)
+                {
+                    char val = junto[reach];
+                    if(val == '+' || val == '-' || val == '(')
+                    {
+                        pilha.push(val);
+                    }
+                    else if(val == '*' || val == '/' || val == '^')
+                    {
+                        while(!pilha.isEmpty() && (pilha.peek() == '*' || pilha.peek() == '/' || pilha.peek() == '^'))
+                        {
+                            posfixa[aux++] = pilha.pop();
+                        }
+                        pilha.push(val);
+                    }
+                    else if(val == ')')
+                    {
+                        while (!pilha.isEmpty() && pilha.peek() != '(')
+                        {
+                            posfixa[aux++] = pilha.pop();
+                        }
+                        pilha.pop();
+                    }
+                    else
+                    {
+                        posfixa[aux++] = val;
+                    }
+                    reach++;
+                }
+                while(!pilha.isEmpty())
+                {
+                    posfixa[aux++] = pilha.pop();
+                }
+                System.out.println("Expressao pos-fixa:");//Imprimi a expressao na forma pos-fixa
+                for (int l = 0; l < aux; l++)
+                {
+                    System.out.print(posfixa[l] + " ");
+                }
+                System.out.println("");
+            }
+        }
+    }
+}
